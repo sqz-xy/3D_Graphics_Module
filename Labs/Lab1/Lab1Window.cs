e718f07ab4365c7737dee5508223483102ef4b09
Your Name <tblakebeet@gmail.com>
2022-01-31 12:50:14 +0000

    Changed the colour to green

--- Labs/Lab1/Lab1Window.cs
+++ Labs/Lab1/Lab1Window.cs
@@ -28,7 +28,8 @@ namespace Labs.Lab1
 
         protected override void OnLoad(EventArgs e)
         {
-            GL.ClearColor(Color4.White);
+            GL.ClearColor(Color4.Green);
+
 
             float[] vertices = new float[] { -0.8f, -0.8f,
                                              -0.8f, 0.8f,
