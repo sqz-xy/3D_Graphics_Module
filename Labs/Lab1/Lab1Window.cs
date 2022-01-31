fe555bc4539a2371c7aa153d6f9f3b833092795b
Your Name <tblakebeet@gmail.com>
2022-01-31 15:00:57 +0000

    House progress Under way with bottom wall

--- Labs/Lab1/Lab1Window.cs
+++ Labs/Lab1/Lab1Window.cs
@@ -8,7 +8,7 @@ namespace Labs.Lab1
 {
     public class Lab1Window : GameWindow
     {
-        private int mVertexBufferObjectID;
+        private int[] mVertexBufferObjectIDArray = new int[2];
         private ShaderUtility mShader;
 
         //TODO: TAKE NOTE OF CULLING, WINDING 
@@ -41,23 +41,67 @@ namespace Labs.Lab1
             //                                  0.8f, 0.8f }; // Top Right corner, Triangle 2
 
 
-            float[] vertices = new float[] { 
-                                 -0.4f, 0.0f,
-                                  0.4f, 0.0f,
-                                  0.0f, 0.6f,
-                                 -0.4f, 0.0f,
-                                 -0.8f, -0.6f,
-                                 0.0f, -0.6f,
-                                 0.4f, 0.0f,
-                                 0.0f, -0.6f,
-                                 0.8f, -0.6f
+            // Triforce
+            //float[] vertices = new float[] { 
+            //                     -0.4f, 0.0f,
+            //                      0.4f, 0.0f,
+            //                      0.0f, 0.6f,
+            //                     -0.8f, -0.6f,
+            //                     0.0f, -0.6f,
+            //                     0.8f, -0.6f
 
 
+            //};
+
+            //uint[] indices = new uint[] { 0,1,2,
+            //                              3,4,0,
+            //                              4,5,1};
+
+            // House
+            float[] vertices = new float[] {
+                                 -0.8f, 0.2f, //0
+                                 -0.4f, 0.2f, //1
+                                 -0.4f, 0.6f, //2
+                                  0.8f, 0.2f, //3
+                                  0.4f, 0.6f, //4
+                                  0.4f, 0.2f, //5
+                                  0.0f, 0.6f, //6
+                                  0.0f, 0.8f, //7
+                                 -0.2f, 0.6f, //8
+                                 -0.2f, 0.8f, //9                           
+                                 -0.4f, -0.6f,//10
+                                  -0.6f, 0.2f,//11
+                                 -0.6f, -0.6f,//12
+                                   0.2f,-0.6f,//13
+                                    0.2f,-0.2f//14
+
+                                    //reuse 10 and 14 i think
             };
 
-            GL.GenBuffers(1, out mVertexBufferObjectID);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectID);
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertices.Length * sizeof(float)), vertices, BufferUsageHint.StaticDraw);
+            //The index of the vertices 0 in the indices corresponds to element 0 in the vertices
+            //Reusing 0 in the indices array reuses that vertex
+            uint[] indices = new uint[] { 0,1,2,
+                                          3,4,5,
+                                          0,5,2,
+                                          6,7,8,
+                                          4,2,5,
+                                          9,8,7,
+                                          1,11,10,
+                                          10,11,12,
+                                          10,13,14
+                                          
+
+                                          
+
+
+                                          
+                                          }; //0,5,2,
+
+            GL.GenBuffers(2, mVertexBufferObjectIDArray);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectIDArray[0]);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertices.Length * sizeof(float)), vertices,
+            BufferUsageHint.StaticDraw);
 
             int size;
             GL.GetBufferParameter(BufferTarget.ArrayBuffer, BufferParameterName.BufferSize, out size);
@@ -65,6 +109,18 @@ namespace Labs.Lab1
             if (vertices.Length * sizeof(float) != size)
             {
                 throw new ApplicationException("Vertex data not loaded onto graphics card correctly");
+            }
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, mVertexBufferObjectIDArray[1]);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indices.Length * sizeof(uint)),
+            indices, BufferUsageHint.StaticDraw);
+
+            GL.GetBufferParameter(BufferTarget.ElementArrayBuffer, BufferParameterName.BufferSize, out
+            size);
+
+            if (indices.Length * sizeof(uint) != size)
+            {
+                throw new ApplicationException("Index data not loaded onto graphics card correctly");
             }
 
             #region Shader Loading Code - Can be ignored for now
@@ -79,8 +135,8 @@ namespace Labs.Lab1
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
-            GL.Clear(ClearBufferMask.ColorBufferBit);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, mVertexBufferObjectIDArray[0]);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, mVertexBufferObjectIDArray[1]);
 
             // shader linking goes here
             #region Shader linking code - can be ignored for now
@@ -93,7 +149,8 @@ namespace Labs.Lab1
             #endregion
 
             // Second argument is where to start the draw, Third argument is the end of the draw
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 9);
+             
+GL.DrawElements(PrimitiveType.Triangles, 64, DrawElementsType.UnsignedInt, 0);
 
             this.SwapBuffers();
         }
@@ -101,7 +158,7 @@ namespace Labs.Lab1
         protected override void OnUnload(EventArgs e)
         {
             base.OnUnload(e);
-            GL.DeleteBuffers(1, ref mVertexBufferObjectID);
+            GL.DeleteBuffers(2, mVertexBufferObjectIDArray);
             GL.UseProgram(0);
             mShader.Delete();
         }
