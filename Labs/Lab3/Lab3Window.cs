ae7f5f9d10f9e9c5b03d332addb0a16e07bfad9c
Tom Beet <tblakebeet@gmail.com>
2022-02-21 12:48:03 +0000

    Added directional light

--- Labs/Lab3/Lab3Window.cs
+++ Labs/Lab3/Lab3Window.cs
@@ -41,8 +41,8 @@ namespace Labs.Lab3
             int vPositionLocation = GL.GetAttribLocation(mShader.ShaderProgramID, "vPosition");
             int vNormalLocation = GL.GetAttribLocation(mShader.ShaderProgramID, "vNormal");
 
-            int uLightDirectionLocation = GL.GetUniformLocation(mShader.ShaderProgramID, "uLightDirection");
-            Vector3 normalisedLightDirection, lightDirection = new Vector3(-1, -1, -1);
+            int uLightDirectionLocation = GL.GetUniformLocation(mShader.ShaderProgramID, "uLightLocation");
+            Vector3 normalisedLightDirection, lightDirection = new Vector3(2, -1, -8.5f);
             Vector3.Normalize(ref lightDirection, out normalisedLightDirection);
             GL.Uniform3(uLightDirectionLocation, normalisedLightDirection);
 
